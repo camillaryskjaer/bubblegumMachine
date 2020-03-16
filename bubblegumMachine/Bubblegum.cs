f710a1e0467e9aa9fc64fe63beb0081fd811c1a1
mathias kamp <math260d@zbc.dk>
2020-03-16 23:39:39 +0100

    added properties

--- bubblegumMachine/Bubblegum.cs
+++ bubblegumMachine/Bubblegum.cs
@@ -2,6 +2,35 @@
 {
     class Bubblegum
     {
+		private char name;
 
+		public char Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private char color;
+
+		public char Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+
+		private char flavor;
+
+		public char Flavor
+		{
+			get { return flavor; }
+			set { flavor = value; }
+		}
+
+        public Bubblegum(char name, char color, char flavor)
+        {
+            this.Color = color;
+            this.flavor = flavor;
+            this.name = name;
+        }
     }
 }
